bd684865999b26abb1cd5e6166e0306d651d2aa6
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-12 23:09:30 +0000

    update: added options to flexsearch

--- vitepress/docs/src/pages/vite.config.ts
+++ vitepress/docs/src/pages/vite.config.ts
@@ -2,9 +2,16 @@
 import { SearchPlugin } from "vitepress-plugin-search";
 import { defineConfig } from "vite";
 
+// https://github.com/nextapps-de/flexsearch#options
+var flexSearchIndexOptions = {
+    preset: "score",
+    cache: 1000,
+    context: true,
+}
+
 //default options
 var options = {
-    /* ...flexSearchIndexOptions, */
+    ...flexSearchIndexOptions,
     previewLength: 62,
     buttonLabel: "Search",
     placeholder: "Search docs",
