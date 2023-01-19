36726b8839bfbc7861f9f44ce18a69d8f9725a75
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-19 20:34:22 +0000

    chore: fix archive file and refactor some things

--- vitepress/docs/src/static/image_settings/index.ts
+++ vitepress/docs/src/static/image_settings/index.ts
@@ -160,6 +160,75 @@ const image_settings = {
         caption: "Much better!",
         max_width: "max-width: 500px;"
     },
+    fox_ir_build_instructions_one: {
+        url: "https://i.imgur.com/WyoVdcR.jpg",
+        alt: "'img'",
+        caption: "698ohm resistors and V2 PCBs",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_two: {
+        url: "https://i.imgur.com/sNB6ju2.png",
+        alt: "'img'",
+        caption: "V2",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_three: {
+        url: "https://i.imgur.com/Ap8OAWY.png",
+        alt: "'img'",
+        caption: "The green markings and notched corners mark the positive sides of the LEDs pictured above.",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_four: {
+        url: "https://i.imgur.com/ayQ5vkf.jpg",
+        alt: "'img'",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_five: {
+        url: "https://i.imgur.com/DpFwecO.jpg",
+        alt: "'img'",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_six: {
+        url: "https://i.imgur.com/50ydMrl.jpg",
+        alt: "'img'",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_seven: {
+        url: "https://i.imgur.com/Qq3cvxi.jpg",
+        alt: "'img'",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_eight: {
+        url: "https://i.imgur.com/yWGaKaC.jpg",
+        alt: "'img'",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_nine: {
+        url: "https://i.imgur.com/Ql4zYCq.jpg",
+        alt: "'img'",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_ten: {
+        url: "https://i.imgur.com/Xrczoyv.jpg",
+        alt: "'img'",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_eleven: {
+        url: "https://i.imgur.com/KKgP8qs.jpg",
+        alt: "'img'",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_twelve: {
+        url: "https://i.imgur.com/SvbHAuY.jpg",
+        alt: "'img'",
+        caption: "The LED is flipped in this Image, the green dot should face AWAY from the PCB.",
+        max_width: "max-width: 400px;"
+    },
+    fox_ir_build_instructions_twelve: {
+        url: "https://i.imgur.com/gopjVG3.png",
+        alt: "'drawing'",
+        max_width: "max-width: 500px;"
+    },
 };
 
 export { image_settings }
